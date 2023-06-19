8dab3f5a688f623f3208f3f8536f3e5fc7e665bd
Daniel Mateu <danielmateu86@gmail.com>
2023-06-19 18:56:50 +0200

    Provider Done!

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import { Figtree } from 'next/font/google'
 import { Sidebar } from '../components/Sidebar';
 import { SupabaseProvider } from '@/providers/SupabaseProvider';
+import { UserProvider } from '@/providers/UserProvider';
 
 
 const font = Figtree({ subsets: ['latin'] })
@@ -20,9 +21,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <SupabaseProvider>
-          <Sidebar >
-            {children}
-          </Sidebar>
+          <UserProvider>
+            <Sidebar >
+              {children}
+            </Sidebar>
+          </UserProvider>
         </SupabaseProvider>
       </body>
     </html>
