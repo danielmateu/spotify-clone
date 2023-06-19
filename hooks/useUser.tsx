1a66e04eb02710702cdc2ca689e0b6eebbf7d408
Daniel Mateu <danielmateu86@gmail.com>
2023-06-19 18:52:28 +0200

    useUser Hook done!

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -1,7 +1,75 @@
+import { User } from '@supabase/supabase-js';
+import { userDetails, Subscription } from '../types';
+import { createContext, useEffect, useState } from 'react';
+import { useSessionContext, useUser as useSupaUser } from '@supabase/auth-helpers-react';
 
-
-export const useUser = () => {
-    return {
-        
-    }
+type UserContextType = {
+    accessToken: string | null;
+    user: User | null;
+    userDetails: userDetails | null;
+    isLoading: boolean;
+    subscription: Subscription | null
 }
+
+export const UserContext = createContext<UserContextType | undefined>(undefined)
+
+export interface Props {
+    [propName: string]: any;
+}
+
+export const MyUserContextProvider = (props: Props) => {
+    const {
+        session,
+        isLoading: isLoadingUser,
+        supabaseClient: supabase
+    } = useSessionContext()
+
+    const user = useSupaUser()
+    const accessToken = session?.access_token ?? null
+
+    const [isLoadingData, setIsLoadingData] = useState(false)
+    const [userDetails, setUserDetails] = useState<userDetails | null>(null)
+    const [subscription, setSubscription] = useState<Subscription | null>(null)
+
+    const getUserDetails = supabase.from('users').select('*').single()
+    const getSubscription = supabase.from('subscriptions').select('*, prices(*, products(*))').in('status', ['trialing', 'active']).single()
+
+    useEffect(() => {
+        if (user && !isLoadingData && !userDetails && !subscription) {
+            setIsLoadingData(true)
+
+            Promise.allSettled([getUserDetails, getSubscription]).then((results) => {
+                const userDeailsPromise = results[0]
+                const subscriptionPromise = results[1]
+
+                if (userDeailsPromise.status === 'fulfilled') {
+                    setUserDetails(userDeailsPromise.value.data as userDetails)
+                }
+
+                if (subscriptionPromise.status === 'fulfilled') {
+                    setSubscription(subscriptionPromise.value.data as Subscription)
+                }
+
+                setIsLoadingData(false)
+            })
+        } else if (!user && !isLoadingUser && !isLoadingData) {
+            setUserDetails(null)
+            setSubscription(null)
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [user, isLoadingUser])
+
+    const value = {
+        accessToken,
+        user,
+        userDetails,
+        isLoading: isLoadingUser || isLoadingData,
+        subscription
+    };
+
+    return <UserContext.Provider value={value} {...props} />;
+}
+
+
+
+
--- types.ts
+++ types.ts
@@ -1,5 +1,4 @@
 import Stripe from "stripe";
-import { metadata } from './app/layout';
 
 export interface userDetails {
     id: string;
@@ -9,6 +8,30 @@ export interface userDetails {
     avatar_url: string;
     billing_address: Stripe.Address;
     payment_method: Stripe.PaymentMethod[Stripe.PaymentMethod.Type];
+}
+
+export interface Product {
+    id: string;
+    active?: boolean;
+    name?: string;
+    description?: string;
+    image?: string;
+    metadata?: Stripe.Metadata;
+}
+
+export interface Price {
+    id: string;
+    product_id: string;
+    active?: boolean;
+    description?: string;
+    unit_amount?: number;
+    currency?: string;
+    type?: Stripe.Price.Type;
+    interval?: Stripe.Price.Recurring.Interval;
+    interval_count?: number;
+    trial_period_days?: number;
+    metadata?: Stripe.Metadata;
+    products?: Product;
 }
 
 export interface Subscription {
@@ -24,4 +47,7 @@ export interface Subscription {
     current_period_end: string;
     ended_at?: string;
     canceled_at?: string;
+    trial_start?: string;
+    trial_end?: string;
+    prices?: Price;
 }
