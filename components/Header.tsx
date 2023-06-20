32e3e89b9c190781b027a0c31bcbe1bed5964e25
Daniel Mateu <danielmateu86@gmail.com>
2023-06-20 16:04:32 +0200

    registro y autenticación

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,10 @@ import { HiHome } from "react-icons/hi";
 import { HiSearch } from "react-icons/hi";
 import { Button } from "./Button";
 import { useAuthModal } from "@/hooks/useAuthModal";
-
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useUser } from "@/hooks/useUser";
+import { FaUser } from "react-icons/fa";
+import { toast } from "react-hot-toast";
 interface HeaderProps {
     children?: React.ReactNode;
     className?: string;
@@ -19,8 +22,21 @@ export const Header: React.FC<HeaderProps> = ({ children, className }) => {
     const authModal = useAuthModal()
     const router = useRouter();
 
-    const handleLogout = () => {
+    const supabaseClient = useSupabaseClient()
+
+    const { user } = useUser()
+
+    const handleLogout = async () => {
         // Handle Logout in the future
+        const { error } = await supabaseClient.auth.signOut()
+        // TODO: Reset any playing songs
+        router.refresh()
+
+        if (error) {
+            toast.error(error.message)
+        } else {
+            toast.success("Logged out successfully")
+        }
     }
     return (
         <div
@@ -79,24 +95,47 @@ export const Header: React.FC<HeaderProps> = ({ children, className }) => {
                 <div
                     className="flex gap-2 items-center justify-center"
                 >
-                    <>
-                        <div>
-                            <Button
-                                onClick={authModal.onOpen}
-                                className="bg-transparent text-neutral-300 font-medium"
+                    {
+                        user ? (
+                            <div
+                                className="flex gap-2 items-center justify-center"
                             >
-                                Sign up
-                            </Button>
-                        </div>
-                        <div>
-                            <Button
-                                onClick={authModal.onOpen}
-                                className="bg-white px-4 py-2 text-black font-medium"
-                            >
-                                Log in
-                            </Button>
-                        </div>
-                    </>
+                                <Button
+                                    onClick={handleLogout}
+                                    className="bg-white px-6 py-2 transition"
+                                >Logout</Button>
+
+                                <Button
+                                    className="rounded-full p-2 items-center hover:opacity-75 transition-opacity"
+                                    onClick={() => router.push(`/account`)}
+                                >
+                                    <FaUser
+                                        size={25}
+                                        className="text-black"
+                                    />
+                                </Button>
+                            </div>
+                        ) : (
+                            <>
+                                <div>
+                                    <Button
+                                        onClick={authModal.onOpen}
+                                        className="bg-transparent text-neutral-300 font-medium"
+                                    >
+                                        Sign up
+                                    </Button>
+                                </div>
+                                <div>
+                                    <Button
+                                        onClick={authModal.onOpen}
+                                        className="bg-white px-4 py-2 text-black font-medium"
+                                    >
+                                        Log in
+                                    </Button>
+                                </div>
+                            </>
+                        )
+                    }
                 </div>
             </div>
             {children}
