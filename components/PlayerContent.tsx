b0c0a290cd400952d8a4a61c03314b8c0c71f055
Daniel Mateu <danielmateu86@gmail.com>
2023-06-22 15:42:02 +0200

    New custom hook and player content created

---
+++ components/PlayerContent.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { Song } from "@/types"
+
+interface PlayerContentProps {
+    song: Song;
+    songUrl: string;
+
+}
+
+export const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
+    return (
+        <div className="
+        
+        "
+        >
+
+        </div>
+    )
+}
